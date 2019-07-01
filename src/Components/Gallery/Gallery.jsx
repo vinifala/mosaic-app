309c305c83b240d232ac4d231c8f4ee634492d63
Vini Falavigna <vini.df@gmail.com>
2019-07-01 09:06:30 -0500

    [FEAT] use react hooks in Gallery.jsx

---
+++ src/Components/Gallery/Gallery.jsx
@@ -0,0 +1,144 @@
+import * as React from 'react';
+import propTypes from 'prop-types';
+import axios from 'axios';
+import Spin from 'antd/lib/spin';
+import 'antd/lib/spin/style/index.css';
+
+import Button from '../antd/Button';
+import Alert from '../antd/Alert';
+import Image from '../Image/Image';
+
+const pageFetchReducer = (state, action) => {
+  switch (action.type) {
+    case 'GALLERY_INIT':
+      return {
+        ...state,
+        loading: true,
+        errorMessage: '',
+      };
+    case 'GALLERY_SUCCESS':
+      return {
+        ...state,
+        loading: false,
+        timestamp: new Date().getTime(),
+        errorMessage: '',
+        photos: action.payload,
+      };
+    case 'GALLERY_FAIL':
+      return {
+        ...state,
+        loading: false,
+        errorMessage: 'Oops, looks like something went wrong',
+      };
+    default:
+      throw new Error();
+  }
+};
+
+const useFetchPages = subreddit => {
+  const [page, setPage] = React.useState(1);
+  const [state, dispatch] = React.useReducer(pageFetchReducer, {
+    errorMessage: '',
+    loading: false,
+    photos: [],
+    timestamp: new Date().getTime(),
+    page,
+  });
+
+  React.useEffect(() => {
+    const fetchPhotos = async () => {
+      dispatch({ type: 'GALLERY_INIT' });
+
+      try {
+        const result = await axios.get(
+          `http://localhost:3001/gallery/${subreddit}/${page}`,
+        );
+
+        dispatch({ type: 'GALLERY_SUCCESS', payload: result.data });
+      } catch (error) {
+        dispatch({ type: 'GALLERY_FAIL' });
+      }
+    };
+
+    fetchPhotos();
+  }, [page]);
+
+  const fetchPrevPage = () => {
+    setPage(page - 1);
+  };
+
+  const fetchNextPage = () => {
+    setPage(page + 1);
+  };
+
+  return [{ ...state, page }, [fetchPrevPage, fetchNextPage]];
+};
+
+function Gallery(props) {
+  const [
+    { photos, loading, errorMessage, timestamp, page },
+    [fetchPrevPage, fetchNextPage],
+  ] = useFetchPages(props.subreddit);
+
+  return (
+    <div>
+      {!loading && !errorMessage && <h2>Plese select an image</h2>}
+      <div
+        style={{
+          display: 'flex',
+          flexWrap: 'wrap',
+          alignContent: 'center',
+          placeContent: 'center',
+          height: '150px',
+        }}
+      >
+        {loading && (
+          <div>
+            <Spin size="large" tip="loading..." />
+          </div>
+        )}
+        {errorMessage && (
+          <div>
+            <Alert message={errorMessage} type="error" showIcon />
+            <Button onClick={() => {}}>Retry</Button>
+          </div>
+        )}
+        {photos.length > 0 &&
+          !loading &&
+          photos.map(photo => (
+            /*
+              Loading the image from browser cache onto the canvas causes the canvas to be tainted:
+              `Failed to execute 'getImageData' on 'CanvasRenderingContext2D':
+              The canvas has been tainted by cross-origin data`
+              including a timestamp as a query string when requesting the image forces the browser
+              to download the image every time.
+            */
+            <Image
+              {...props}
+              key={photo.id}
+              src={`${photo.link}?t=${timestamp}`}
+            />
+          ))}
+      </div>
+      {!loading && !errorMessage && (
+        <div>
+          <Button onClick={fetchPrevPage} disabled={page < 2}>
+            &lt; Previous
+          </Button>
+          <span> Page {page} </span>
+          <Button onClick={fetchNextPage}>Next &gt;</Button>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Gallery;
+
+Gallery.defaultProps = {
+  subreddit: 'earthporn',
+};
+
+Gallery.propTypes = {
+  subreddit: propTypes.string,
+};
